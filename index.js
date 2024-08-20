68d6a85d059d409eba89dc3a15b45420b400f7a5
AlissonRodrigues <rodrigues101112@gmail.com>
2024-08-20 17:32:28 -0300

    adicionar popup funcoes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function openPopup() {
   popup.style.display = "block";
 }
 editbutton.addEventListener("click", openPopup);
-
+// fechar popup
 function closePopup() {
   container.style.display = "none";
   popup.style.display = "none";
@@ -34,3 +34,27 @@ function addNames(event) {
   closePopup();
 }
 buttonSave.addEventListener("click", addNames);
+
+const popupEdit = document.querySelector(".popup-edit");
+const buttonEdit = document.querySelector(".profile__add");
+const popupContainer = document.querySelector(".popup__container-edit");
+const buttonCloseEdit = document.querySelector(".popup__close-edit");
+
+//  abrir o popup para adicionar imagens
+function openPopupEdit() {
+  popupEdit.style.display = "block";
+  popupContainer.style.display = "block";
+}
+buttonEdit.addEventListener("click", openPopupEdit);
+
+// fechar o popup
+function closePopupEdit() {
+  popupEdit.style.display = "none";
+  popupContainer.style.display = "none";
+}
+buttonCloseEdit.addEventListener("click", closePopupEdit);
+
+// adicionando imagens e tema
+
+// curtindo o coração
+const heart = document.querySelector("");
