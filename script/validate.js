07cc572bdadab86164b3e431f9d118110027ee6d
AlissonRodrigues <rodrigues101112@gmail.com>
2024-09-14 11:52:17 -0300

    updating form validation file

--- script/validate.js
+++ script/validate.js
@@ -12,43 +12,31 @@ function removeErrorMessage(input, errorMessage, config) {
   input.classList.remove(config.inputInvalidErrorMessage);
 }
 // habilitar o botao
-function enableButton(item, config) {
-  if (item == "name" || item == "description") {
-    const button = document.querySelector(config.popupSaveButton);
-    button.classList.remove("form__button_disabled");
-    button.removeAttribute("disabled");
-  }
-  if (item == "title" || item == "url") {
-    const button = document.querySelector(config.formAddButton);
-    button.classList.remove("form__button_disabled");
-    button.removeAttribute("disabled");
-  }
+function enableButton(form, config) {
+  const button = form.querySelector(config.popupSaveButton);
+  button.classList.remove("form__button_disabled");
+  button.removeAttribute("disabled");
 }
 // disabilidar o botao
-function disableButton(item, config) {
-  if (item == "name" || item == "description") {
-    const button = document.querySelector(config.popupSaveButton);
-    button.classList.add("form__button-disabled");
-    button.setAttribute("disabled", true);
-  }
-  if (item == "title" || item == "url") {
-    const button = document.querySelector(config.formAddButton);
-    button.classList.add("form__button-disabled");
-    button.setAttribute("disabled", true);
-  }
+function disableButton(form, config) {
+  const button = form.querySelector(config.popupSaveButton);
+  button.classList.add("form__button-disabled");
+  button.setAttribute("disabled", true);
 }
 
-function checkIsValid(event, config) {
+function checkIsValid(form, event, config) {
   const input = event.target;
   const isValid = input.validity.valid && !/^\s*$/.test(input.value);
   const errorMessage = input.validationMessage;
 
   if (!isValid) {
     addErrorMessage(input, errorMessage, config);
-    disableButton(input.id, config);
+    disableButton(form, config);
   } else {
     removeErrorMessage(input, errorMessage, config);
-    enableButton(input.id, config);
+  }
+  if (form.checkValidity()) {
+    enableButton(form, config);
   }
 }
 
@@ -58,7 +46,9 @@ function enableValidation(config) {
   for (const form of forms) {
     const inputs = Array.from(form.querySelectorAll(config.inputElement));
     for (const input of inputs) {
-      input.addEventListener("input", (event) => checkIsValid(event, config));
+      input.addEventListener("input", (event) =>
+        checkIsValid(form, event, config)
+      );
     }
   }
 }
